3af67e1dd2d585f39273595ca61e0f0413db2cf1
Friendly-Neighborhood <68468538+Friendly-Neighborhood@users.noreply.github.com>
2023-03-24 01:42:35 +0200

    Create connect.php

---
+++ src/partials/connect.php
@@ -0,0 +1,16 @@
+<?php
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  $conn = new mysqli('localhost','root','','test');
+  if($conn->connect_error){
+    die('Connection failed : '.$conn->connect_error);
+  }else{
+    $stmt = $conn->prepare("insert into register(name, email, password) values(?,?,?)");
+    $stmt->bind_param("sss", $name, $email, $password);
+    $stmt->execute();
+    echo "Registered!";
+    $stmt->close();
+    $conn->close();
+?>
